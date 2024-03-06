6edc2f5f9a2238dbd01cc29dafc0b1e58f214124
NilsRamstoeck <nilsramstoeck@gmail.com>
2024-03-06 20:14:46 +0000

    add github action

--- example/worker.d.ts
+++ example/worker.d.ts
@@ -4,3 +4,8 @@ declare module '*.worker.ts' {
   const content: import('esbuild-worker-plugin').WorkerConstructor;
   export default content;
 }
+
+declare module '*.worker.js' {
+  const content: import('esbuild-worker-plugin').WorkerConstructor;
+  export default content;
+}
